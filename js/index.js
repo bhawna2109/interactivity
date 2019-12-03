c3006f28cebe263ca0e2c7b6043265561c440424
Bhawna Agarwal <bhawna2109@users.noreply.github.com>
2019-12-03 11:02:05 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener(
+      'keydown', function(e){
+            console.log(e);
+
+            if(e.key == "j"){
+                  console.log("hello");
+            }
+      }
+)
